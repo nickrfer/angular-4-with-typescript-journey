3bc5abad14da34ba039c8a9aee31e84b6d277743
Nicolas Ferreira <nickrfer@gmail.com>
2018-07-01 08:34:06 -0300

    Adding events.

---
+++ angular-essentials/src/app/user.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <input type="text" (input)="onUserInput($event)">
+    <p>Hello! {{ name }}</p>
+    <p>I'm the user component</p>
+  `
+})
+export class UserComponent {
+  name = 'Nick';
+
+  onUserInput(event) {
+    this.name = event.target.value;
+  }
+}
