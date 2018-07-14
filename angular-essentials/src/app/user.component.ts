775cbab507e85d345096e9e45d6b945909cce727
Nicolas Ferreira <nickrfer@gmail.com>
2018-07-14 13:29:01 -0300

    Custom events

--- angular-essentials/src/app/user.component.ts
+++ angular-essentials/src/app/user.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   template: `
-    <input type="text" (input)="onUserInput($event)">
+    <input type="text" (input)="onUserInput($event)" [value]="name">
+    <!--<input type="text" [(ngModel)]="name">-->
     <p>Hello! {{ name }}</p>
     <p>I'm the user component</p>
+    <app-user-detail></app-user-detail>
   `
 })
 export class UserComponent {
-  name = 'Nick';
+  @Input() name;
+  @Output() nameChanged = new EventEmitter<string>();
 
   onUserInput(event) {
-    this.name = event.target.value;
+    //this.name = event.target.value;
+    this.nameChanged.emit(event.target.value);
   }
 }
